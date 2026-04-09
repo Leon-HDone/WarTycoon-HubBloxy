f6ae4fb5ec67c01811c71f256902240ec0165a68
DevDesert <jentschleon0@gmail.com>
2026-04-09 16:51:06 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/04ea249a7da046d9a307c7a675eee0e1.lua"))()
+loadstring(game:HttpGet("https://paste-webside.pages.dev/raw/WarTycoon"))()
